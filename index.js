fa4b7915dbe2481d1b0d6c64efc8bd1edd18b255
meraki11 <ericksonsarahl@gmail.com>
2021-01-25 15:32:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var itemPrice = 15;
+var amtOfMoneyInWallet = 25;
+var numberOfFriends = 35;
+var ageInYears = 45;
+var firstName = "Penn";
+var lastName = "Ulum";
+var middleInitial = "D";
+
+console.log('New amount of money in wallet after purchase is: ' + amtOfMoneyInWallet - itemPrice);
+
+console.log('Number of friends made per year on earth is: ' + ageInYears / numberOfFriends);
+
+console.log('I am known as: ' + "firstName" + "middleInitial" + "lastName");
