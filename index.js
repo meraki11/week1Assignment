edea527cce0e0a2393c00d1587a0a458b58a1288
meraki11 <ericksonsarahl@gmail.com>
2021-01-26 16:11:37 -0700

    fine tuning

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
-
 var itemPrice = 15;
 var amtOfMoneyInWallet = 25;
-var numberOfFriends = 35;
+var numberOfFriends = 90;
 var ageInYears = 45;
 var firstName = "Penn";
 var lastName = "Ulum";
 var middleInitial = "D";
 
 console.log('New amount of money in wallet after purchase is: ' + (amtOfMoneyInWallet - itemPrice));
-console.log('Number of friends made per year on earth is: ' + ageInYears / numberOfFriends);
-console.log('I am known as: ' + firstName + middleInitial + lastName);
+
+console.log('Number of friends made per year on earth is: ' + numberOfFriends / ageInYears);
+
+console.log('I am known as: ' + firstName + " " + middleInitial + " " + lastName);
