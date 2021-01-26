b6bc95919068631c19ac55e05c9ac8c85e3c8620
meraki11 <ericksonsarahl@gmail.com>
2021-01-26 15:57:09 -0700

    finally spelled right

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var itemPrice = 15;
 var amtOfMoneyInWallet = 25;
 var numberOfFriends = 35;
@@ -8,4 +10,4 @@ var middleInitial = "D";
 
 console.log('New amount of money in wallet after purchase is: ' + (amtOfMoneyInWallet - itemPrice));
 console.log('Number of friends made per year on earth is: ' + ageInYears / numberOfFriends);
-console.log('I am known as: ' + "firstName" + "middleInitial" + "lastName");
+console.log('I am known as: ' + firstName + middleInitial + lastName);
