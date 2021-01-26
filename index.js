46295a6ffc1ad2c7ec6e464f13bef079d63610c2
meraki11 <ericksonsarahl@gmail.com>
2021-01-26 14:55:24 -0700

    spacing test

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ var firstName = "Penn";
 var lastName = "Ulum";
 var middleInitial = "D";
 
-console.log('New amount of money in wallet after purchase is: ' + amtOfMoneyInWallet - itemPrice);
+console.log('New amount of money in wallet after purchase is: ' + (amtOfMoneyInWallet - itemPrice));
 console.log('Number of friends made per year on earth is: ' + ageInYears / numberOfFriends);
 console.log('I am known as: ' + "firstName" + "middleInitial" + "lastName");
