15f0223a332b2580a5c0dc0b85b82c98fe8a7181
meraki11 <ericksonsarahl@gmail.com>
2021-01-25 17:04:14 -0700

    testing

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var ageInYears = 45;
 var firstName = "Penn";
 var lastName = "Ulum";
 var middleInitial = "D";
-
+console.log('Hello World');
 console.log('New amount of money in wallet after purchase is: ' + amtOfMoneyInWallet - itemPrice);
-
 console.log('Number of friends made per year on earth is: ' + ageInYears / numberOfFriends);
-
 console.log('I am known as: ' + "firstName" + "middleInitial" + "lastName");
